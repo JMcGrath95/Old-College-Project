fc1b1c3aaa30e26fdbc07d9e4d9d33cfb445ad6b
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2021-05-19 19:14:35 +0100

    Small change to player movement

--- Assets/Shane/Scripts/PlayerMovement.cs
+++ Assets/Shane/Scripts/PlayerMovement.cs
@@ -50,29 +50,30 @@ public class PlayerMovement : MonoBehaviour
         //Update animator.
         animator.SetFloat("MovementSpeed", directionToMoveThisFrame.magnitude);
 
-
+        //Idle.
         if (directionToMoveThisFrame == Vector3.zero)
         {
             playerState = PlayerState.Idle;
+
         }
+        //Walking.
         else
         {
             playerState = PlayerState.Walking;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionToMoveThisFrame), rotationSpeed);
         }
-
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            animator.SetBool("Attack1", true);
-            playerState = PlayerState.Attacking;
-        }
     }
 
     private void UpdateMovementDirection()
     {
-        //directionToMoveThisFrame = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
+#if UNITY_EDITOR || UNITY_STANDALONE
+        directionToMoveThisFrame = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+#endif
+#if UNITY_ANDROID || UNITY_IOS
         directionToMoveThisFrame = new Vector3(fixedJoystick.Horizontal, 0, fixedJoystick.Vertical);
+#endif
+
         directionToMoveThisFrame = mainCamera.transform.TransformDirection(directionToMoveThisFrame);
         directionToMoveThisFrame = new Vector3(directionToMoveThisFrame.x, 0, directionToMoveThisFrame.z).normalized * movementSpeed * Time.deltaTime;
     }
