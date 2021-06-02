f4cf9012b6924287560130d368527314f54f9c17
Bartlomiej Bialowas <46888832+GenericUsername337@users.noreply.github.com>
2021-06-02 18:33:25 +0100

    game quits when player dies. added URP package

--- Assets/Bartek/Scripts/GameController.cs
+++ Assets/Bartek/Scripts/GameController.cs
@@ -10,9 +10,15 @@ public class GameController : MonoBehaviour
 
     public void StartGame()
     {
+        Player.GetComponent<PlayerHealth>().DeathEvent += GameController_DeathEvent;
         NavMeshBuilder.BuildNavMesh();
         Instantiate(Player, new Vector3(0, 0, 0), Quaternion.identity);
         cam.FindPlayer();
+    }
+
+    private void GameController_DeathEvent()
+    {
+        GameOver();
     }
 
     public void GameOver()
