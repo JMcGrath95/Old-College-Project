16ebf544a89e497de65660d91ee1f669a5e45005
PiotrGolk <47738716+PiotrGolk@users.noreply.github.com>
2021-06-30 18:43:37 +0200

    Fixed movement issue

--- Assets/Peter/Scripts/AI/AI_Boss.cs
+++ Assets/Peter/Scripts/AI/AI_Boss.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum Boss_States 
+{
+    Spitting,
+    Rolling,
+    Flipped,
+    Leaping
+}
+
 public class AI_Boss : MonoBehaviour
 {
+    Boss_States currentstate = Boss_States.Spitting;
     public _boss Boss;
     public GameObject Player;
-    float timeleft = 0f;
-    float timeleftroll = 3f;
+    //float timeleft = 0f;
+    public ProjectileEffect bullet;
+    
     bool IsRolling = false;
     bool HitWall = false;
+    bool CanFire = true;
     bool IsFlipped = false;
     public Transform BulletSource;
     Room room;
@@ -18,36 +29,56 @@ public class AI_Boss : MonoBehaviour
     Rigidbody rigid_body;
     Vector3 currenttarget;
     Collider _collider;
+    float shoot_count=0;
     // Start is called before the first frame update
     void Start()
     {
     }
     void Update()
     {
-             
-        if (timeleftroll <= 0f && !IsRolling)
-        {            
-            LookTowards();
-            StartCoroutine(RollTo());
-        }
-
-        if (!IsFlipped)
+        //Nova Spit(Bart's Effects)             <<
+        //Roll                                  ##
+        //Flips on the back after wall hit      ##
+        //Gets up                               ##
+        //Jumps up                              ##
+        //Lands in the middle                   >>
+        switch (currentstate)
         {
-            //Set to invincible.
+            case Boss_States.Spitting:
+                LookTowards();
+                if (shoot_count <= 5 && CanFire==true)
+                {
+                    StartCoroutine(Fire());
+                }
+                else if (shoot_count > 5)
+                {
+                    currentstate = Boss_States.Rolling;
+                }
+                //Effect
+                break;
+            case Boss_States.Rolling:
+                if (!IsRolling)
+                {
+                    StartCoroutine(RollTo());                    
+                }
+                Rotation();
+                Move(currenttarget);
+                break;
+            case Boss_States.Flipped:
+                if (!IsFlipped)
+                {
+                    StartCoroutine(Flip());
+                }
+                //Flip
+                break;
+            case Boss_States.Leaping:
+                StartCoroutine(Leaping());
+                shoot_count = 0f; 
+                //Leap
+                break;
+            default:
+                break;
         }
-
-        if (Vector3.Distance(Player.transform.position, transform.position) <= Boss.AttackRange && !IsRolling)
-        {
-            LookTowards();
-            Fire();
-        }
-
-        if (IsRolling)
-        {
-            rigid_body.MoveRotation(rigid_body.rotation * Quaternion.Euler(new Vector3(180, 0, 0) * Time.fixedDeltaTime));
-        }
-
-        timeleftroll -= Time.deltaTime;
     }
 
     private void Awake()
@@ -59,11 +90,25 @@ public class AI_Boss : MonoBehaviour
         rigid_body = GetComponent<Rigidbody>();        
         boss_agent = GetComponent<NavMeshAgent>();
         _collider = GetComponent<Collider>();
+        //bullet.InitializeEffect(BulletSource.gameObject);
+        //currentstate = Boss_States.Spitting;
+    }
+    IEnumerator Flip() 
+    {   
+        IsFlipped = true;
+        StopRigidBody();        
+        yield return new WaitForSeconds(5f);
+        IsFlipped = false;
+        currentstate = Boss_States.Leaping;
     }
     private void Health_DeathEvent()
     {
         room.roomCleared = true;
         room.OpenDoors();
+    }
+    void Rotation() 
+    {
+        rigid_body.MoveRotation(rigid_body.rotation * Quaternion.Euler(new Vector3(180, 0, 0) * Time.fixedDeltaTime * Boss.Speed*0.5f));
     }
     void LookTowards()
     {
@@ -71,31 +116,54 @@ public class AI_Boss : MonoBehaviour
         Vector3 NewDir = Vector3.RotateTowards(transform.forward, targetDirection, 10 * Time.deltaTime, 0.0f);
         transform.rotation = Quaternion.LookRotation(NewDir);
     }
-    void Fire()
-    {
-        timeleft -= Time.deltaTime;
-        if (timeleft <= 0)
-        {
-            Vector3 shootDir = ((Player.transform.position + new Vector3(0, 0.5f)) - BulletSource.position);
-            GameObject Temp = Instantiate(Boss.Projectile, BulletSource.position,Quaternion.identity);
-            AI_Bullet bullet = Temp.GetComponent<AI_Bullet>();
-            bullet.SetDir(shootDir, BulletSource.position, Boss.AttackRange, (int)Boss.Attack, Boss.AttackSpeed, BulletSource);
-            timeleft = 0.5f;
-        }
-    }
+    //void Fire()
+    //{
+    //    timeleft -= Time.deltaTime;
+    //    if (timeleft <= 0)
+    //    {
+    //        Vector3 shootDir = ((Player.transform.position + new Vector3(0, 0.5f)) - BulletSource.position);
+    //        GameObject Temp = Instantiate(Boss.Projectile, BulletSource.position,Quaternion.identity);
+    //        AI_Bullet bullet = Temp.GetComponent<AI_Bullet>();
+    //        bullet.SetDir(shootDir, BulletSource.position, Boss.AttackRange, (int)Boss.Attack, Boss.AttackSpeed, BulletSource);
+    //        timeleft = 0.5f;
+    //    }
+    //}
     IEnumerator RollTo()
     {
         _collider.isTrigger = true;
+        rigid_body.isKinematic = false;
         currenttarget = (Player.transform.position - transform.position).normalized;
         Vector3 Player_pos = Player.transform.position;
         IsRolling = true;
-        rigid_body.AddForce(new Vector3(currenttarget.x, 0f ,currenttarget.z) * 180f , ForceMode.Force);
-        
-        yield return new WaitUntil(() => HitWall == true);        
-        timeleftroll = 3f;        
+        //rigid_body.AddForce(new Vector3(currenttarget.x, 0f ,currenttarget.z) * 180f , ForceMode.Force);
+        //rigid_body.MovePosition(transform.position + currenttarget * Time.deltaTime * 1000);        
+        LookTowards();        
+        yield return new WaitUntil(() => HitWall == true);
         IsRolling = false;
         _collider.isTrigger = false;
-        HitWall = false;
+        IsFlipped = false;
+        StopRigidBody();
+        HitWall = false;        
+        currentstate = Boss_States.Flipped;
+        
+    }
+    IEnumerator Fire() 
+    {
+        //bullet.TriggerEffect(); 
+        CanFire = false;
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Fired!!!!" + shoot_count);
+        CanFire = true;
+        shoot_count++;
+    }
+    IEnumerator Leaping() 
+    {
+        rigid_body.isKinematic = true;
+        transform.position = room.transform.position + new Vector3(0, 2.5f, 0);
+        currentstate = Boss_States.Spitting;
+        StopRigidBody();
+        yield return new WaitUntil(() => Vector3.Distance(room.floor.transform.position, transform.position) <= 0.2f);
+        rigid_body.isKinematic = false;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -113,12 +181,24 @@ public class AI_Boss : MonoBehaviour
         else if (other.gameObject.tag=="Wall")
         {
             HitWall = true;
+            StopRigidBody();
         }
         else if (other.gameObject.GetComponent<Hallway>() != null)
         {
+            StopRigidBody();
             HitWall = true;
         }   
     }
+    void StopRigidBody() 
+    {        
+        rigid_body.velocity = new Vector3(0,0,0);
+        rigid_body.angularVelocity = new Vector3(0, 0, 0);
+        rigid_body.isKinematic = true;
+    }
 
-
+    void Move(Vector3 player_pos) 
+    {
+        Vector3 _player_pos = player_pos;
+        rigid_body.MovePosition(transform.position + _player_pos * Time.deltaTime * Boss.Speed);
+    }
 }
