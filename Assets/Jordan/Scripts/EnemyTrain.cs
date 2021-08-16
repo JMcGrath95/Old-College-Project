6c819866d4bcd62eb8c7e11c30031e1dce2bd43b
S00189902 <46888710+S00189902@users.noreply.github.com>
2021-08-16 21:03:05 +0100

    train facing right direction, collider attached, other carriages added as children working on timing and properly resetting

--- Assets/Jordan/Scripts/EnemyTrain.cs
+++ Assets/Jordan/Scripts/EnemyTrain.cs
@@ -6,11 +6,11 @@ public class EnemyTrain : MonoBehaviour
 {
     GameObject Player;
     public ChargeLine chargeLinePrefab;
-   
+    public float timer;
     MeshRenderer mesh;
-    Vector3 pPos;
-    float Starttime, ToDis;
-    bool repeat = false;
+    Vector3 pPos,starpos= new Vector3(-1000,-1000,-1000);
+    float Starttime;
+    bool reset = false;
     public float speed = 1.0f;
     
      
@@ -18,15 +18,19 @@ public class EnemyTrain : MonoBehaviour
    
     IEnumerator Start ()
     {
+        gameObject.transform.position = starpos;
         Starttime = Time.time;
-        
+       
         FindPlayerPos();
-            yield return SpawnLine();
+        yield return SpawnLine(timer);
         
         
     }
     private void Update()
     {
+        
+        
+            StartCoroutine(ResetAll());
         
     }
     public void FindPlayerPos()
@@ -40,12 +44,12 @@ public class EnemyTrain : MonoBehaviour
 
 
     }
-    IEnumerator SpawnLine()
+    IEnumerator SpawnLine(float time)
     {
         ChargeLine c = Instantiate(chargeLinePrefab,pPos,Quaternion.Euler(new Vector3(90,Random.Range(0f,360f),0)));
         Vector3 a = c.Point1.transform.position;
         Vector3 b = c.Point2.transform.position;
-        ToDis = Vector3.Distance(c.Point1.transform.position, c.Point2.transform.position);
+        yield return new WaitForSeconds(time);
         StartCoroutine(RunCharge(2,a,b));
         yield return null;
     }
@@ -53,6 +57,7 @@ public class EnemyTrain : MonoBehaviour
     
     IEnumerator RunCharge(float time,Vector3 a,Vector3 b)
     {
+        
         if(GameObject.FindGameObjectWithTag("Line"))
         {
             ChargeLine c = GameObject.FindGameObjectWithTag("Line").GetComponent<ChargeLine>();
@@ -61,16 +66,34 @@ public class EnemyTrain : MonoBehaviour
             while(i < 1.0f)
             {
                 gameObject.transform.LookAt(c.Point2.transform.position);
+                gameObject.transform.Rotate(new Vector3(0, 180, 0));
                 i += Time.deltaTime * rate;
                 this.transform.position = Vector3.Lerp(a, b, i);
                 
                 yield return null;
             }
+            reset = true;
         }
         else
         {
             yield return null;
         }
     }
-    
+    IEnumerator ResetAll()
+    {
+        if(reset)
+        {
+            gameObject.transform.position = starpos;
+            Destroy(GameObject.FindGameObjectWithTag("Line"));
+            reset = false;
+        }
+        yield return null;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
 }
