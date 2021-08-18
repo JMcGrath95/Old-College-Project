c0dad6b1c59345133b74ce1b55f4b6e7b56ee80b
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2021-08-18 17:41:42 +0100

    QuestGiver Script created

---
+++ Assets/David/Scripts/Quests/QuestGiver.cs
@@ -0,0 +1,12 @@
+using DaveCore.UI.Objectives.Task;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DaveCore.UI.Objectives
+{
+    public class QuestGiver : MonoBehaviour
+    {
+        [SerializeField] Quest quest;
+    }
+}
